34d2d9d34ed90006365bb612fb268c0b8cc347cd
vermavikasakash <vikas26061995@gmail.com>
2026-06-24 15:29:28 +0530

    redis implementation for caching

--- server/redis-client.js
+++ server/redis-client.js
@@ -5,7 +5,7 @@ const redisClient = createClient({
   password: process.env.REDIS_PASSWORD,
   socket: {
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    port: Number(process.env.REDIS_PORT),
   },
 });
 
